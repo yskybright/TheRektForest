be7bc32e1c247cf2c813e9a653e6f80d6c0588ab
yskybright <rgh99@naver.com>
2023-12-28 15:56:27 +0900

    [Feat] StartScene , Lobby
    - 기존의 StartScene을 Lobby로 수정
    - 새로운 StartScene 생성

---
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyUI : MonoBehaviour
+{
+    public GameObject StartSceneButtons;
+    public GameObject Status;
+    public GameObject Inventory;
+
+    void Start()
+    {
+        Status.SetActive(false);
+        Inventory.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStatus()
+    {
+        StartSceneButtons.SetActive(false);
+        Status.SetActive(true);
+    }
+
+    public void OnClickInventory()
+    {
+        StartSceneButtons.SetActive(false);
+        Inventory.SetActive(true);
+    }
+
+    public void OnClickQuit()
+    {
+        Status.SetActive(false);
+        Inventory.SetActive(false);
+        StartSceneButtons.SetActive(true);
+    }
+
+    public void OnClickDungeon()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+}
--- Assets/Scripts/UI/StartSceneUI.cs
+++ Assets/Scripts/UI/StartSceneUI.cs
@@ -5,43 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class StartSceneUI : MonoBehaviour
 {
-    public GameObject StartSceneButtons;
-    public GameObject Status;
-    public GameObject Inventory;
+    public GameObject GoLobbyBtn;
+    public GameObject GameIntroBtn;
+    public GameObject SettingBtn;
 
+    // Start is called before the first frame update
     void Start()
     {
-        Status.SetActive(false);
-        Inventory.SetActive(false);
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
         
     }
 
-    public void OnClickStatus()
+    public void OnClickGoLobby()
     {
-        StartSceneButtons.SetActive(false);
-        Status.SetActive(true);
+        SceneManager.LoadScene("Lobby");
     }
-
-    public void OnClickInventory()
-    {
-        StartSceneButtons.SetActive(false);
-        Inventory.SetActive(true);
-    }
-
-    public void OnClickQuit()
-    {
-        Status.SetActive(false);
-        Inventory.SetActive(false);
-        StartSceneButtons.SetActive(true);
-    }
-
-    public void OnClickDungeon()
-    {
-        SceneManager.LoadScene("GameScene");
-    }
-
 }
